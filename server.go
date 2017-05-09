79c3f48bb86ab57f39bd4eda8e8b85f92a8e6598
Garrett Thornburg <film42@gmail.com>
2017-05-09 17:14:23 -0400

    Shut down the listener before worker routine

--- server.go
+++ server.go
@@ -8,17 +8,19 @@ import (
 )
 
 type Server struct {
-	waitGroup            *sync.WaitGroup
-	listener             net.Listener
-	acceptedConnsChannel chan net.Conn
-	shuttingDown         bool
+	waitGroup                *sync.WaitGroup
+	listener                 net.Listener
+	acceptedConnsChannel     chan net.Conn
+	listenerWasClosedChannel chan bool
+	shuttingDown             bool
 }
 
 func NewServer() *Server {
 	return &Server{
-		waitGroup:            &sync.WaitGroup{},
-		shuttingDown:         false,
-		acceptedConnsChannel: make(chan net.Conn),
+		waitGroup:                &sync.WaitGroup{},
+		shuttingDown:             false,
+		acceptedConnsChannel:     make(chan net.Conn),
+		listenerWasClosedChannel: make(chan bool),
 	}
 }
 
@@ -35,25 +37,38 @@ func (server *Server) ListenAndServe(port int) {
 
 	logger.Info.Println("Server started on", listenOn)
 
+	server.waitGroup.Add(1)
+	defer server.waitGroup.Done()
+
 	server.createListenLoop()
 
 	for {
-		go server.handleConnection(<-server.acceptedConnsChannel)
+		select {
+		case <-server.listenerWasClosedChannel:
+			return
+		case conn := <-server.acceptedConnsChannel:
+			go server.handleConnection(conn)
+		}
 	}
+
 }
 
 func (server *Server) Shutdown() {
 	// We send a message to ensure we never break the listener.
 	server.shuttingDown = true
+	// Close the listener
+	server.listener.Close()
 	server.waitGroup.Wait()
 }
 
 func (server *Server) createListenLoop() {
 	go func() {
+		server.waitGroup.Add(1)
+		defer server.waitGroup.Done()
+
 		for {
 			if server.shuttingDown {
-				server.listener.Close()
-				return
+				break
 			}
 
 			conn, err := server.listener.Accept()
@@ -64,6 +79,8 @@ func (server *Server) createListenLoop() {
 
 			server.acceptedConnsChannel <- conn
 		}
+
+		server.listenerWasClosedChannel <- true
 	}()
 }
 
